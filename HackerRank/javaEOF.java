0688d5c1b64a974030034ad76556d9be97abc697
Nikhil Mettupally <snc0417@gmail.com>
2018-12-23 18:39:33 -0600

    Create javaEOF.java

---
+++ HackerRank/javaEOF.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int i = 1;
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            System.out.println(i+" "+in.nextLine());
+            i++;
+        }
+    }
+}
+
