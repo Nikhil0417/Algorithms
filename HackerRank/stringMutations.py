0fe063171e9bc5cd3fb59b526cea34740eb5fd39
Nikhil Mettupally <snc0417@gmail.com>
2019-01-11 17:57:31 -0600

    Create stringMutations.py

---
+++ HackerRank/stringMutations.py
@@ -0,0 +1,13 @@
+def mutate_string(string, position, character):
+    # l = list(string)
+    # l[position] = character
+    # string = ''.join(l)   #METHOD-1
+
+    string = string[:position] + character + string[position+1:]    #METHOD-2
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
