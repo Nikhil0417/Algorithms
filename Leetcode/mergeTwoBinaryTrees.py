915723721c8e4b9ca1ebe9635ace2a0b802fb58a
Nikhil Mettupally <snc0417@gmail.com>
2019-02-01 12:53:43 -0600

    Create mergeTwoBinaryTrees.py

---
+++ Leetcode/mergeTwoBinaryTrees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        #t3 = TreeNode(None)
+        if(t1 != None and t2 != None):
+            t1.val = t1.val + t2.val
+            #if(t1.left != None and t2.left == None):
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            return t1
+        else:
+            return t1 or t2
+                
